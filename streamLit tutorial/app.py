d7773df0c24863df1fb27b9a76094063fe2d24ee
MirandaTN <115226584+MirandaTN@users.noreply.github.com>
2023-12-16 01:51:21 +0200

    Updated UI app.py

--- streamLit tutorial/app.py
+++ streamLit tutorial/app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-
+import os
 
 st.set_page_config(page_title= "1st Streamlit", page_icon=":tada" ,layout="wide")
 
-st.subheader("Welcome to your FAV test prep tool :)")
 st.title("TestPrepTool")
-st.write("Create a folder to store notes/ slides per module")
+st.subheader("Welcome to your FAV test prep tool :star:")
 
-import os
-import streamlit as st
+st.write("Your are required to create a folder for a subject, provide a descriptive name of the subject."+
+         "Upload slides and study notes. Upon doing so, a quiz will be generated from the files you uploaded !! ")
+
 
 # Function to create a folder
 def create_folder(folder_name):
@@ -43,24 +43,25 @@ def delete_file(folder_name, file_name):
         st.error(f"File '{file_name}' not found in folder '{folder_name}'.")
 
 # Streamlit UI
-st.title("Folder and File Management")
 
 # Create Folder
 st.header("Create Folder")
 new_folder_name = st.text_input("Enter folder name:")
 if st.button("Create Folder"):
     create_folder(new_folder_name)
+    
 
 # Select Folder for File Addition
-st.header("Add File to Folder")
+    st.header("Add File to Folder")
 selected_folder = st.selectbox("Select a folder:", os.listdir(), key="add_file_selectbox")
 if selected_folder:
     uploaded_file = st.file_uploader("Choose a file:", type=["txt", "pdf", "jpg", "png"])
     if st.button("Add File"):
-        add_file(selected_folder, uploaded_file)
+        add_file(selected_folder, uploaded_file)  
 
+  
 # Select Folder for File Deletion
-st.header("Delete File from Folder")
+    st.header("Delete File from Folder")
 delete_folder = st.selectbox("Select a folder:", os.listdir(), key="delete_file_selectbox")
 if delete_folder:
     files_to_delete = st.multiselect("Select files to delete:", os.listdir(delete_folder))
@@ -68,10 +69,4 @@ if delete_folder:
         for file_to_delete in files_to_delete:
             delete_file(delete_folder, file_to_delete)
 
-# List of Folders
-st.header("List of Folders")
-folder_list = os.listdir()
-if folder_list:
-    st.write(folder_list)
-else:
-    st.info("No folders created yet.")
+
